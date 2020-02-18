253086c88e7b35e7d59a4e084c40747893a33359
Jonathan Neugebauer <jonathan.n@uni-muenster.de>
2020-02-18 17:58:57 +0100

    Restructure code to run as service

--- media_server/SIPWavePlayer.py
+++ media_server/SIPWavePlayer.py
@@ -1,3 +1,4 @@
+import logging
 from datetime import datetime
 from threading import Event
 
@@ -14,27 +15,89 @@ from sipsimple.streams.rtp.audio import AudioStream
 from sipsimple.threading.green import run_in_green_thread
 
 from media_server import config
+import redis
+import time
+import threading
 
 class SIPWavePlayer(SIPApplication):
-    def __init__(self):
+    def __init__(self, run_event):
         SIPApplication.__init__(self)
+        self.run_event = run_event
         self.ended = Event()
         self.callee = None
-        self.player = None
-        self.wave_file = 'audio/%s' % config.WAV_FILE
         self.session = None
+        self.player = None
         notification_center = NotificationCenter()
         notification_center.add_observer(self)
         self.account_manager = AccountManager()
+        self.start(FileStorage('config'))
+        self.wave_file = 'audio/%s' % config.WAV_FILE
+        self.redis_connection = redis.Redis(host=config.REDIS_HOST, port=config.REDIS_PORT, db=config.REDIS_DB)
 
+    def next_call(self):
+        while self.run_event.is_set():
+            # Has to be a valid SIP URI (e. g. 'sip:123@ipbx.local')
+            callee = None
+
+            if self.redis_connection.exists(config.REDIS_INDEX):
+                callee = self.redis_connection.lpop(config.REDIS_INDEX)
+
+            if not callee:
+                logging.info('No callees to call - sleeping for %s second' % config.SLEEP_TIME)
+                time.sleep(config.SLEEP_TIME)
+                continue
+        
+            logging.info('Calling %s' % callee)
+            self.call(callee)
+            break
+
+    @run_in_green_thread
     def call(self, target):
         self.callee = target
+
+        try:
+            self.callee = ToHeader(SIPURI.parse(self.callee))
+        except SIPCoreError:
+            print 'Specified SIP URI is not valid'
+            #self.stop()
+            return
         
-        if self.state is None:
-            self.start(FileStorage('config'))
+        try:
+            routes = DNSLookup().lookup_sip_proxy(self.callee.uri, ['udp']).wait()
+        except DNSLookupError, e:
+            print 'DNS lookup failed: %s' % str(e)
+        else:
+            self.session = Session(self.account)
+            self.session.connect(self.callee, routes, [AudioStream()])
+
+    def _NH_SIPSessionDidStart(self, notification):
+        logging.info('SIPSessionDidStart')
+        session = notification.sender
+        audio_stream = session.streams[0]
+        audio_stream.bridge.add(self.player)
+        self.player.play()
+
+    def _NH_SIPSessionWillEnd(self, notification):
+        logging.info('SIPSessionWillEnd')
+        session = notification.sender
+        audio_stream = session.streams[0]
+        self.player.stop()
+        audio_stream.bridge.remove(self.player)
+
+    def _NH_WavePlayerDidEnd(self, notification):
+        logging.info('WavePlayerDidEnd')
+        self.session.end()
+        #self.stop()
+    
+    def _NH_SIPSessionDidEnd(self, notification):
+        logging.info('SIPSessionDidEnd')
+        self.next_call()
+        #self.stop()
 
     @run_in_green_thread
     def _NH_SIPApplicationDidStart(self, notification):
+        logging.info('SIPApplicationDidStart')
+
         self.account_manager.load()
 
         if not self.account_manager.has_account(config.ACCOUNT_ID):
@@ -54,21 +117,9 @@ class SIPWavePlayer(SIPApplication):
         settings.audio.input_device = None
         settings.audio.output_device = None
         settings.save()
+
         self.player = WavePlayer(SIPApplication.voice_audio_mixer, self.wave_file, loop_count=3, initial_delay=1,
                                  pause_time=1)
-        try:
-            self.callee = ToHeader(SIPURI.parse(self.callee))
-        except SIPCoreError:
-            print 'Specified SIP URI is not valid'
-            self.stop()
-            return
-        try:
-            routes = DNSLookup().lookup_sip_proxy(self.callee.uri, ['udp']).wait()
-        except DNSLookupError, e:
-            print 'DNS lookup failed: %s' % str(e)
-        else:
-            self.session = Session(self.account)
-            self.session.connect(self.callee, routes, [AudioStream()])
 
     def _NH_SIPSessionGotRingIndication(self, notification):
         print 'Ringing!'
@@ -100,30 +151,9 @@ class SIPWavePlayer(SIPApplication):
     def _NH_SIPAccountRegistrationDidEnd(self, notification):
         print '%s Registration ended.\n' % datetime.now().replace(microsecond=0)
 
-    def _NH_SIPSessionDidStart(self, notification):
-        print 'Session started!'
-        session = notification.sender
-        audio_stream = session.streams[0]
-        audio_stream.bridge.add(self.player)
-        self.player.play()
-
     def _NH_SIPSessionDidFail(self, notification):
         print 'Failed to connect'
-        self.stop()
-
-    def _NH_WavePlayerDidEnd(self, notification):
-        print 'Finished playing'
-        self.stop()
-
-    def _NH_SIPSessionWillEnd(self, notification):
-        session = notification.sender
-        audio_stream = session.streams[0]
-        self.player.stop()
-        audio_stream.bridge.remove(self.player)
-
-    def _NH_SIPSessionDidEnd(self, notification):
-        print 'Session ended'
-        self.stop()
+        #self.stop()
 
     def _NH_SIPApplicationDidEnd(self, notification):
         self.ended.set()
--- media_server/__main__.py
+++ media_server/__main__.py
@@ -3,35 +3,25 @@ import time
 import redis
 from media_server import config
 from .SIPWavePlayer import SIPWavePlayer
+import threading
 
 def main():
     logging.basicConfig(level=config.LOGLEVEL, format='%(asctime)s [%(levelname)s] %(message)s')
 
-    logging.info('Connecting to redis')
-    redis_connection = redis.Redis(host=config.REDIS_HOST, port=config.REDIS_PORT, db=config.REDIS_DB)
+    run_event = threading.Event()
+    run_event.set()
 
-    while True:
-        # Has to be a valid SIP URI (e. g. 'sip:123@ipbx.local')
-        callee = None
+    application = SIPWavePlayer(run_event)
+    application.next_call()
 
-        if redis_connection.exists(config.REDIS_INDEX):
-            callee = redis_connection.lpop(config.REDIS_INDEX)
-
-        if not callee:
-            logging.info('No callees to call - sleeping for %s second' % config.SLEEP_TIME)
-            time.sleep(config.SLEEP_TIME)
-            continue
-        
-        logging.info('Calling %s' % callee)
-        
-        player = SIPWavePlayer()
-        player.call(callee)
-        if player.session:
-            player.session.end()
-        player.ended.wait()
-        del(player)
-
-        logging.info('Call to %s ended' % callee)
+    try:
+        while True:
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        logging.info("KeyboardInterrupt received. Attempting to close SIP application...")
+        run_event.clear()
+        application.stop()
+        logging.info("SIP application closed successfully")
 
 if __name__ == '__main__':
     main()
